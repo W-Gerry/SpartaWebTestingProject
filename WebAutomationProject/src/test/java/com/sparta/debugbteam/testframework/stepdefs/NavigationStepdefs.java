0b569eace2ecf2e6d504947ae3cd8f14b202649b
Jack Birchall <jackbirchall55@gmail.com>
2024-04-05 16:02:30 +0100

    Added stepdefs.NavigationStepdefs class and navigation.feature resource.

---
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/stepdefs/NavigationStepdefs.java
@@ -0,0 +1,74 @@
+package com.sparta.debugbteam.testframework.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NavigationStepdefs {
+    @Given("I am on the Luma website")
+    public void iAmOnTheLumaWebsite() {
+    }
+
+    // Navigate to the homepage
+    @When("I navigate to the homepage")
+    public void iNavigateToTheHomepage() {
+    }
+
+    @Then("I should see the homepage")
+    public void iShouldSeeTheHomepage() {
+    }
+
+    // Navigate to the What's New page
+    @When("I navigate to the What's New page")
+    public void iNavigateToTheWhatSNewPage() {
+    }
+
+    @Then("I should see the What's New page")
+    public void iShouldSeeTheWhatSNewPage() {
+    }
+
+    // Navigate to the Women page
+    @When("I navigate to the Women page")
+    public void iNavigateToTheWomenPage() {
+    }
+
+    @Then("I should see the Women page")
+    public void iShouldSeeTheWomenPage() {
+    }
+
+    // Navigate to the Men page
+    @When("I navigate to the Men page")
+    public void iNavigateToTheMenPage() {
+    }
+
+    @Then("I should see the Men page")
+    public void iShouldSeeTheMenPage() {
+    }
+
+    // Navigate to the Gear page
+    @When("I navigate to the Gear page")
+    public void iNavigateToTheGearPage() {
+    }
+
+    @Then("I should see the Gear page")
+    public void iShouldSeeTheGearPage() {
+    }
+
+    // Navigate to the Training page
+    @When("I navigate to the Training page")
+    public void iNavigateToTheTrainingPage() {
+    }
+
+    @Then("I should see the Training page")
+    public void iShouldSeeTheTrainingPage() {
+    }
+
+    // Navigate to the Sale page
+    @When("I navigate to the Sale page")
+    public void iNavigateToTheSalePage() {
+    }
+
+    @Then("I should see the Sale page")
+    public void iShouldSeeTheSalePage() {
+    }
+}
