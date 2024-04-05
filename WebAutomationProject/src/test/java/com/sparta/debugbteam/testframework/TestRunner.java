067151bb9bf97d67b4c997a4d6a419fd98507363
John Herbert <jherbert@spartaglobal.com>
2024-04-05 16:48:48 +0100

    Added the test runner class and testframework package for everyones use

--- WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/TestRunner.java
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.sparta.debugbteam.testframework",
+        glue = "com.sparta.jh.testframework",
         plugin = {"pretty", "html:target/cucumber-reports"},
         publish = true
 )
