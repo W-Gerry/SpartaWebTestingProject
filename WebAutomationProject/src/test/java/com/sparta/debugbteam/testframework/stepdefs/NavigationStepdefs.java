5bb0ab6067d47c0846a40da34557955a4c4b702e
Jack Birchall <jackbirchall55@gmail.com>
2024-04-05 16:13:38 +0100

    Implemented setup for NavigationStepdefs

--- WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/stepdefs/NavigationStepdefs.java
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/stepdefs/NavigationStepdefs.java
@@ -1,10 +1,70 @@
 package com.sparta.debugbteam.testframework.stepdefs;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+//import com.sparta.debugbteam.testframework.lib.pages.HomePage;
+//import com.sparta.debugbteam.testframework.lib.pages.PastPage;
+//import com.sparta.debugbteam.testframework.lib.pages.SearchPage;
+//import com.sparta.debugbteam.testframework.lib.pages.LoginPage;
+
+import io.cucumber.java.*;
+import io.cucumber.java.en.*;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.io.File;
+import java.io.IOException;
 
 public class NavigationStepdefs {
+
+    private static final String DRIVER_LOCATION = "src/test/resources/chromedriver-win64/chromedriver.exe";
+    private static final String BASE_URL = "https://news.ycombinator.com/";
+    private static ChromeDriverService service;
+    private WebDriver webDriver;
+
+//    private HomePage homePage;
+//    private PastPage pastPage;
+//    private SearchPage searchPage;
+//    private LoginPage loginPage;
+
+    public static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--start-maximized");
+        options.addArguments("--headless");
+        options.addArguments("--remote-allow-origins=*");
+        return options;
+    }
+
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(DRIVER_LOCATION))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+    @Before
+    public void setup() {
+        webDriver = new RemoteWebDriver(service.getUrl(), getChromeOptions());
+        //webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void afterEach() {
+        webDriver.quit();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        service.stop();
+    }
+
+
+    //---------------------------------------------------------------------------------------------------
+
+
     @Given("I am on the Luma website")
     public void iAmOnTheLumaWebsite() {
     }
