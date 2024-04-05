c51e88067027ed02f32f44a959b90001003019c2
Jack Birchall <jackbirchall55@gmail.com>
2024-04-05 16:20:18 +0100

    Added & implemented HomePage.java class.

---
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/HomePage.java
@@ -0,0 +1,19 @@
+package com.sparta.debugbteam.testframework.lib.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private final WebDriver webDriver;
+
+    public HomePage(WebDriver webDriver) {
+        if (!webDriver.getTitle().equals("Hacker News")) {
+            throw new IllegalStateException(
+                    "This is not the Hacker News home page, current page is: "
+                            + webDriver.getCurrentUrl());
+        }
+        this.webDriver = webDriver;
+    }
+}
