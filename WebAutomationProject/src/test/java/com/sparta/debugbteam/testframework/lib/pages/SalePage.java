7beabd4ba53173f69aaae9bd62177c8be2137665
Jack Birchall <jackbirchall55@gmail.com>
2024-04-08 10:23:10 +0100

    Updated SalePage.java class.

--- WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/SalePage.java
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/SalePage.java
@@ -1,4 +1,29 @@
 package com.sparta.debugbteam.testframework.lib.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class SalePage {
+    private final WebDriver webDriver;
+    public SalePage(WebDriver webDriver) {
+        if (!webDriver.getTitle().contains("Sale")) {
+            throw new IllegalStateException("This is not the Sale page," +
+                    " current page is: " + webDriver.getCurrentUrl());
+
+        }
+        this.webDriver = webDriver;
+    }
+    public String getUrl() {
+        return webDriver.getCurrentUrl();
+    }
+
+    public String getTitle() {
+        return webDriver.getTitle();
+    }
+
+    public HomePage goToHomePage() {
+        webDriver.findElement(By.linkText("home")).click();
+        return new HomePage(webDriver);
+    }
+
 }
