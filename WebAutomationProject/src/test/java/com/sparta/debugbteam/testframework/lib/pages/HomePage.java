32a98f34fb9c033c0c141c28bfa7303374d5832b
Jack Birchall <jackbirchall55@gmail.com>
2024-04-08 10:33:57 +0100

    Added goTo... methods in HomePage.java class.

--- WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/HomePage.java
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/HomePage.java
@@ -1,5 +1,6 @@
 package com.sparta.debugbteam.testframework.lib.pages;
 
+import io.cucumber.java.da.Men;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -20,6 +21,36 @@ public class HomePage {
     public HomePage goToHomePage() {
         webDriver.findElement(By.linkText("home")).click();
         return new HomePage(webDriver);
+    }
+
+    public TrainingShopPage goToTrainingShopPage() {
+        webDriver.findElement(By.linkText("Training")).click();
+        return new TrainingShopPage(webDriver);
+    }
+
+    public GearShopPage goToGearShopPage() {
+        webDriver.findElement(By.linkText("Gear")).click();
+        return new GearShopPage(webDriver);
+    }
+
+    public SalePage goToSalePage() {
+        webDriver.findElement(By.linkText("Sale")).click();
+        return new SalePage(webDriver);
+    }
+
+    public WomenShopPage goToWomenShopPage() {
+        webDriver.findElement(By.linkText("Women")).click();
+        return new WomenShopPage(webDriver);
+    }
+
+    public MenShopPage goToMenShopPage() {
+        webDriver.findElement(By.linkText("Men")).click();
+        return new MenShopPage(webDriver);
+    }
+
+    public WhatsNewPage goToWhatsNewPage() {
+        webDriver.findElement(By.linkText("What's New")).click();
+        return new WhatsNewPage(webDriver);
     }
 
 }
