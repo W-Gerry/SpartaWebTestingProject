71a1c372a7db35b56ee18941b03cefc045a9bc83
Jack Birchall <jackbirchall55@gmail.com>
2024-04-08 10:25:40 +0100

    Updated WomenShopPage.java class.

--- WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/WomenShopPage.java
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/lib/pages/WomenShopPage.java
@@ -1,4 +1,29 @@
 package com.sparta.debugbteam.testframework.lib.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class WomenShopPage {
+    private final WebDriver webDriver;
+    public WomenShopPage(WebDriver webDriver) {
+        if (!webDriver.getTitle().contains("Women")) {
+            throw new IllegalStateException("This is not the Women's shop page," +
+                    " current page is: " + webDriver.getCurrentUrl());
+
+        }
+        this.webDriver = webDriver;
+    }
+    public String getUrl() {
+        return webDriver.getCurrentUrl();
+    }
+
+    public String getTitle() {
+        return webDriver.getTitle();
+    }
+
+    public HomePage goToHomePage() {
+        webDriver.findElement(By.linkText("home")).click();
+        return new HomePage(webDriver);
+    }
+
 }
