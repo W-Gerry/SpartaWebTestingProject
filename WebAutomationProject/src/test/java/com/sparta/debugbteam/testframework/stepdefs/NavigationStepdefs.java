ed17e5ba57114bccc2a38786fdc14f0e8f0c54b2
Will Gerry <william.gerry@outlook.com>
2024-04-08 14:16:25 +0100

    Update chromeoptions to minimise chrome window during test

--- WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/stepdefs/NavigationStepdefs.java
+++ WebAutomationProject/src/test/java/com/sparta/debugbteam/testframework/stepdefs/NavigationStepdefs.java
@@ -38,7 +38,7 @@ public class NavigationStepdefs {
     public static ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--start-maximized");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--remote-allow-origins=*");
         return options;
     }
